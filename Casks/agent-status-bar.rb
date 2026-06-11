c8292532f4757647cd2c46103438e0fc3e07da4c
Evian <zhu.huibin@qq.com>
2026-06-12 00:31:14 +0800

    Update agent-status-bar to 0.1.9

--- Casks/agent-status-bar.rb
+++ Casks/agent-status-bar.rb
@@ -1,6 +1,6 @@
 cask "agent-status-bar" do
-  version "0.1.8"
-  sha256 "77f6a0a4c49599f9c86ea44fd88fe61cf80327dad523682fd4b23215bb8ab9ed"
+  version "0.1.9"
+  sha256 "5826c963d665da46ccee75501c9183a3483b426558d4c18ccf0d0f475878896e"
 
   url "https://github.com/pku-zhb/agent-status-bar/releases/download/v#{version}/AgentStatusBar-v#{version}.zip"
   name "Agent Status Bar"
