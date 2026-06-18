66a5d10ff882ae07902769f2954777fa3e383eb1
Evian <zhu.huibin@qq.com>
2026-06-18 18:24:54 +0800

    termy-server 1.5.3

--- Formula/termy-server.rb
+++ Formula/termy-server.rb
@@ -1,13 +1,13 @@
 class TermyServer < Formula
   desc "Terminal PTY backend for the Termy Obsidian plugin (魔改 fork)"
   homepage "https://github.com/pku-zhb/Termy"
-  version "1.5.2"
+  version "1.5.3"
   license "GPL-3.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/pku-zhb/Termy/releases/download/1.5.2/termy-server-darwin-arm64"
-      sha256 "47a6b228158164a5b7aac657d5b3b01ef1f155f480d4c9a44cd578a6a8e4b028"
+      url "https://github.com/pku-zhb/Termy/releases/download/1.5.3/termy-server-darwin-arm64"
+      sha256 "2a5e99b86135dbfb15f7040e025f2a3ceec3a04325651d1e9a0ad576a0f11882"
     end
     # Intel 款 Mac 需要在 release 里补一个 termy-server-darwin-x86_64 资产，再加 on_intel 块。
   end
